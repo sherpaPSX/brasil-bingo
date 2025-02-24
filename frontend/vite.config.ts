cf86482eae7cb239cd5807057be603c2ffe75d54
lukaswerner <sherpa.psx@gmail.com>
2025-02-24 14:12:04 +0100

    host fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: Number(process.env.PORT) || 3000, // Use the environment's port or default to 3000
+    host: "0.0.0.0", // povolí přístup z jiných síťových rozhraní
+    allowedHosts: ["brasil-bingo-frontend.onrender.com"], // Přidání hosta do povolených
   },
   resolve: {
     alias: {
