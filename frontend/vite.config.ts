250a0765360ba7980949eb8b1599cf1956c4df27
lukaswerner <sherpa.psx@gmail.com>
2025-02-24 14:00:58 +0100

    fix port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: Number(process.env.PORT) || 3000, // Use the environment's port or default to 3000
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
